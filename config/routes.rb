9295380c2d1bea078900b1b4847fec7549951833
Wei Huang <vveiwei.huang@gmail.com>
2023-03-22 17:13:01 +1100

    well its not bad

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "index", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
